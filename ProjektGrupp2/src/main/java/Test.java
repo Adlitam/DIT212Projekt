cd3c0d11d68bfe47a1f993821e9dfa2b2b66005b
Adlitam <matilda.andersson06@gmail.com>
2015-03-23 16:32:55 +0100

    M

---
+++ ProjektGrupp2/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Matildaandersson on 15-03-23.
+ */
+public class Test {
+
+    public static void main(String args[]){
+
+        System.out.println("Detta är ett test!");
+
+    }
+}
