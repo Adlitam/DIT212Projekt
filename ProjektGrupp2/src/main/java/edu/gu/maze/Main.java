8dd7a518af8cec59fa56c75fe33e66eb73e00f64
Adlitam <matilda.andersson06@gmail.com>
2015-03-24 11:20:20 +0100

    M

--- ProjektGrupp2/src/main/java/edu/gu/maze/Main.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/Main.java
@@ -22,7 +22,7 @@ public final class Main {
                     ProjectController.create(project, projectView);
                     projectView.setVisible(true);
                 });
-        System.out.println("Bla");
+        System.out.println("Bla1");
         System.out.println("Testar git");
 	}
 }
