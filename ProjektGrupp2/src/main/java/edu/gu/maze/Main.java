6daba47d07a56686b005b9f09d462971a64c5432
Adlitam <matilda.andersson06@gmail.com>
2015-04-20 09:08:39 +0200

    test for gui

--- ProjektGrupp2/src/main/java/edu/gu/maze/Main.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/Main.java
@@ -1,26 +1,22 @@
 package edu.gu.maze;
 
-import edu.gu.maze.controller.ProjectController;
-import edu.gu.maze.model.Project;
-import edu.gu.maze.view.ProjectView;
-import javax.swing.SwingUtilities;
+
+import edu.gu.maze.view.MazeMainView;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 
 /*
   Application entry class (if using standard java and Swing)
 */
 public final class Main {
-	private Main() {
-		/* No instances allowed! */
-	}
+
 
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-                    final Project project = new Project();
-                    final ProjectView projectView = new ProjectView(project);
+       new MazeMainView();
+    }
 
-                    
-                    ProjectController.create(project, projectView);
-                    projectView.setVisible(true);
-                });
-	}
+
 }
---
+++ ProjektGrupp2/src/main/java/edu/gu/maze/view/MazeMainView.java
@@ -0,0 +1,57 @@
+package edu.gu.maze.view;
+
+import javafx.application.Application;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.beans.EventHandler;
+
+
+/**
+ * Created by Matildaandersson on 15-04-15.
+ */
+public class MazeMainView extends Application {
+
+
+    Button button;
+
+    public MazeMainView(){
+
+        launch();
+
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Maze");
+
+        FlowPane layout = new FlowPane();
+        /*
+
+        button = new Button();
+        button.setText("Click me!");
+
+        button.setOnAction(new EventHandler(){
+
+            System.out.printLn("Some text");
+
+        });
+
+        */
+
+        Scene scene = new Scene(layout);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+
+    }
+
+
+}
