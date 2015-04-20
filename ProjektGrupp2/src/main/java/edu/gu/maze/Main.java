8f9a0b2afc05b76afa7eb5baaa951e092463b64e
Adlitam <matilda.andersson06@gmail.com>
2015-04-20 10:42:58 +0200

    One dot moving aroundwith keyevent

--- ProjektGrupp2/src/main/java/edu/gu/maze/Main.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/Main.java
@@ -3,9 +3,15 @@ package edu.gu.maze;
 
 import edu.gu.maze.view.MazeMainView;
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 
@@ -14,6 +20,7 @@ import javafx.stage.Stage;
 */
 public final class Main extends Application {
 
+    private static final int POS = 20;
 
 	public static void main(String[] args) {
 
@@ -25,8 +32,9 @@ public final class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         primaryStage.setTitle("Maze");
+        Circle circle = new Circle(50,100,10, Color.RED);
+        Group layout = new Group(new Label(),circle);
 
-        FlowPane layout = new FlowPane();
         /*
 
         button = new Button();
@@ -39,8 +47,20 @@ public final class Main extends Application {
         });
 
         */
+        Scene scene = new Scene(layout,500,500);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override public void handle(KeyEvent event) {
+                switch (event.getCode()) {
+                    case UP:    circle.setCenterY(circle.getCenterY() - POS); break;
+                    case RIGHT: circle.setCenterX(circle.getCenterX() + POS); break;
+                    case DOWN:  circle.setCenterY(circle.getCenterY() + POS); break;
+                    case LEFT:  circle.setCenterX(circle.getCenterX() - POS); break;
+                }
+            }
+        });
 
-        Scene scene = new Scene(layout);
+
+
 
         primaryStage.setScene(scene);
 
