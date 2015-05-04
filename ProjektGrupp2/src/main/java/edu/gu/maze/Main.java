452c6f187a6c9f6f47229dc769a44a224e223182
Omega <wikarin@student.chalmers.se>
2015-05-04 14:47:12 +0200

    Serialized game

--- ProjektGrupp2/src/main/java/edu/gu/maze/Main.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/Main.java
@@ -2,12 +2,14 @@ package edu.gu.maze;
 
 import edu.gu.maze.controller.GameController;
 import edu.gu.maze.model.IGame;
+import edu.gu.maze.model.Game;
 import edu.gu.maze.model.Map;
 import edu.gu.maze.view.MainView;
 import edu.gu.maze.view.MazeMainView;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+import java.io.*;
+import java.util.*;
 
 public final class Main extends Application {
 
@@ -17,6 +19,19 @@ public final class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //read serialized Game object, if present.
+        try(
+                InputStream file = new FileInputStream("Game.ser");
+                InputStream buffer = new BufferedInputStream(file);
+                ObjectInput input = new ObjectInputStream (buffer);
+                )
+        {
+            Game game = (Game)input.readObject();
+        }
+        catch(FileNotFoundException ex){
+            //TODO: This is where a new Game.ser file is made, if none is present. It should
+            //be initialized with all hard-coded maps, but no other information.
+        }
         new MazeMainView(primaryStage);
 
         //model
--- ProjektGrupp2/src/main/java/edu/gu/maze/model/Game.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/model/Game.java
@@ -1,9 +1,10 @@
 package edu.gu.maze.model;
+import java.io.Serializable;
 
 /**
  * Created by Matildaandersson on 15-04-01.
  */
-public class Game implements IGame{
+public class Game implements IGame, Serializable{
     int currentHighScore = 0;
 
     public int getcurrentHighScore(){
