09c9876643e446e82d910d0cd41c33c3187b9b2e
Adlitam <matilda.andersson06@gmail.com>
2015-05-05 18:21:39 +0200

    M

---
+++ ProjektGrupp2/src/main/java/edu/gu/maze/view/MapPane.java
@@ -0,0 +1,19 @@
+package edu.gu.maze.view;
+
+import edu.gu.maze.model.Map;
+
+/**
+ * Created by Matildaandersson on 15-05-04.
+ */
+public class MapPane {
+
+
+
+    int[][] lvl1 = new int[50][90];
+
+
+
+
+
+
+}
