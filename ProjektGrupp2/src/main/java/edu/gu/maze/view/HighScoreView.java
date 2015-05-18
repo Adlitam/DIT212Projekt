207ae8a684a7363753237089043b053638bb552f
Adlitam <matilda.andersson06@gmail.com>
2015-05-18 14:32:45 +0200

    Change in the highscoreview

--- ProjektGrupp2/src/main/java/edu/gu/maze/view/HighScoreView.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/view/HighScoreView.java
@@ -3,6 +3,7 @@ package edu.gu.maze.view;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -13,27 +14,6 @@ import java.beans.PropertyChangeSupport;
  * Created by xiang-yu on 2015-05-10.
  */
 public class HighScoreView{
-    private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-        pcs.addPropertyChangeListener(listener);
-    }
 
-    public void removePropertyChangeListener(PropertyChangeListener listener) {
-        pcs.removePropertyChangeListener(listener);
-    }
-
-    public HighScoreView(Stage stage){
-        stage.setTitle("Maze");
-        BorderPane borderPaneLayout = new BorderPane();
-        HBox hBoxlayout = new HBox();
-        Button backButton = new Button("Back to start");
-        hBoxlayout.getChildren().add(backButton);
-        hBoxlayout.setAlignment(Pos.CENTER);
-        backButton.setOnAction(e ->
-                pcs.firePropertyChange("backButtonH", "value1", "value2"));
-        borderPaneLayout.setBottom(hBoxlayout);
-        Scene highScoreScene = new Scene(borderPaneLayout, 800, 600);
-        stage.setScene(highScoreScene);
-    }
 }
