8168b4c8802927036e3481aa24f7d39823169ac2
Brandtinator <gusrlejo@student.gu.se>
2015-05-25 14:06:15 +0200

    Fixed some yellow warnings in StartController

--- ProjektGrupp2/src/main/java/edu/gu/maze/controller/StartController.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/controller/StartController.java
@@ -8,16 +8,13 @@ import javafx.stage.Stage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-/**
- * Created by Matildaandersson on 15-05-19.
- */
+
 public class StartController implements PropertyChangeListener {
 
     private IGame model;
     private Game map;
     private Stage stage;
     private StartView startView;
-    private Constants cons = new Constants();
 
     public StartController(IGame model, StartView startView, Stage primaryStage){
         this.map = (Game) model;
@@ -36,21 +33,21 @@ public class StartController implements PropertyChangeListener {
                 mainView.addPropertyChangeListener(mainController);
                 break;
             case "Slot1":
-                CreatePlayerView startView = new CreatePlayerView(stage,cons.SLOT1);
+                CreatePlayerView startView = new CreatePlayerView(stage,Constants.SLOT1);
                 CreatePlayerController createPlayerController1 = new CreatePlayerController(model, stage);
                 startView.addPropertyChangeListener(createPlayerController1);
                 break;
             case "Slot2":
-                CreatePlayerView startView2 = new CreatePlayerView(stage,cons.SLOT2);
+                CreatePlayerView startView2 = new CreatePlayerView(stage,Constants.SLOT2);
                 CreatePlayerController createPlayerController2 = new CreatePlayerController(model, stage);
                 startView2.addPropertyChangeListener(createPlayerController2);
                 break;
             case "Slot3":
-                CreatePlayerView startView3 = new CreatePlayerView(stage,cons.SLOT3);
+                CreatePlayerView startView3 = new CreatePlayerView(stage,Constants.SLOT3);
                 CreatePlayerController createPlayerController3 = new CreatePlayerController(model, stage);
                 startView3.addPropertyChangeListener(createPlayerController3);
                 break;
-
+            default:
     }
     }
 }
