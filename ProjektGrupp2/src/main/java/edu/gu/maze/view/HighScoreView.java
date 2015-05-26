f825dd83607a1a64511cf65a44050d10e6800dac
Adlitam <matilda.andersson06@gmail.com>
2015-05-26 13:29:33 +0200

    Created a method in highscore

--- ProjektGrupp2/src/main/java/edu/gu/maze/view/HighScoreView.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/view/HighScoreView.java
@@ -13,6 +13,9 @@ public class HighScoreView{
     private Button backButton;
     private BorderPane borderPaneLayout;
 
+    private Label player, score;
+    private  HBox highScore;
+
     public HighScoreView(Stage stage){
         stage.setTitle("Maze");
         borderPaneLayout = new BorderPane();
@@ -28,47 +31,45 @@ public class HighScoreView{
         Scene highScoreScene = new Scene(borderPaneLayout, 800, 620);
         stage.setScene(highScoreScene);
     }
-
-    public void createList(){
-        //Add three labels
-        Label player1 = new Label("Player 1");
-        player1.setFont(new Font(20));
-        Label player2 = new Label("Player 2");
-        player2.setFont(new Font(20));
-        Label player3 = new Label("Player 3");
-        player3.setFont(new Font(20));
-
-        Label score1 = new Label("Score1");
-        score1.setFont(new Font(20));
-        Label score2 = new Label("Score2");
-        score2.setFont(new Font(20));
-        Label score3 = new Label("Score3");
-        score3.setFont(new Font(20));
-
-        HBox highScore1 = new HBox();
-        highScore1.getChildren().addAll(player1,score1);
-        highScore1.setSpacing(100);
-        highScore1.setAlignment(Pos.CENTER);
-        highScore1.setStyle("-fx-border-color: Black");
-        HBox highScore2 = new HBox();
-        highScore2.getChildren().addAll(player2,score2);
-        highScore2.setSpacing(100);
-        highScore2.setAlignment(Pos.CENTER);
-        highScore2.setStyle("-fx-border-color: Black");
-        HBox highScore3 = new HBox();
-        highScore3.getChildren().addAll(player3,score3);
-        highScore3.setSpacing(100);
-        highScore3.setAlignment(Pos.CENTER);
-        highScore3.setStyle("-fx-border-color: Black");
+  public void createList(){
 
         VBox vBoxCenter = new VBox();
-        //Added the labels to the HBox
-        vBoxCenter.getChildren().addAll(highScore1,highScore2,highScore3);
+
+        for(int i = 0; i < 3; i++){
+            if(i == 0) {
+                createLabel("player1", "score1");
+            }else if(i == 1){
+                createLabel("player2", "score2");
+            }else if(i == 2){
+                createLabel("player3", "score3");
+            }
+            vBoxCenter.getChildren().addAll(highScore);
+
+       }
+
+
         vBoxCenter.setAlignment(Pos.CENTER);
-        vBoxCenter.setSpacing(100);
+      vBoxCenter.setSpacing(100);
         vBoxCenter.setStyle("-fx-border-color: Black");
         //Added the hBox to the layout.
         borderPaneLayout.setCenter(vBoxCenter);
+
+
+    }
+
+    private void createLabel(String playerName, String scorePlayer){
+
+        player = new Label(playerName);
+        player.setFont(new Font(20));
+        score = new Label(scorePlayer);
+        score.setFont(new Font(20));
+        highScore = new HBox();
+        highScore.getChildren().addAll(player,score);
+        highScore.setSpacing(100);
+        highScore.setAlignment(Pos.CENTER);
+        highScore.setStyle("-fx-border-color: Black");
+
+
     }
 
     public void createTop(){
