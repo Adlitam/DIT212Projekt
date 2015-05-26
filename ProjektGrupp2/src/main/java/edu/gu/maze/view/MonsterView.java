2dfabba4ce115f848e11ecb9024295cffbe097c0
ye-xiangyu <treetokyo@mail.com>
2015-05-26 16:10:25 +0200

    changed some views.

--- ProjektGrupp2/src/main/java/edu/gu/maze/view/MonsterView.java
+++ ProjektGrupp2/src/main/java/edu/gu/maze/view/MonsterView.java
@@ -1,7 +1,23 @@
 package edu.gu.maze.view;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-//TODO
 public class MonsterView extends ImageView {
+    private final int xPos;
+    private final int yPos;
+
+    public MonsterView(int x, int y){
+        this.xPos = x;
+        this.yPos = y;
+        Image image = new Image("Monster.png");
+        setImage(image);
+        setFitWidth(30);
+        setFitHeight(30);
+    }
+
+    @Override
+    public String toString(){
+        return "MonsterView, xPos = " + xPos + " yPos = " + yPos;
+    }
 }
